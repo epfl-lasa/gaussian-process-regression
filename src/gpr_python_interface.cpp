1ceff4078d74250e621c9ba1cdf96baa2300c8f2
Klas Kronander <klas.kronander@epfl.ch>
2015-11-04 01:30:39 +0100

    removed boost.numpy completed one of the conversion functions

--- src/gpr_python_interface.cpp
+++ src/gpr_python_interface.cpp
@@ -3,13 +3,13 @@
 #include "gaussian_process_regression/gaussian_process_regression.h"
 
 #include <eigen3/Eigen/Dense>
-#include <numpy/ndarrayobject.h> // do not use this
-#include "boost/numpy/ndarray.hpp" // rather prefer this (require Boost.Numpy (boost_numpy via catkin))
+#include <numpy/ndarrayobject.h> // 
+
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 
 namespace bp = boost::python;
 
-typedef float real;
+typedef double real;
 
 /*minimal example for testing succesful compilation
   of python module*/
@@ -18,15 +18,73 @@ typedef float real;
 // }
 
 // functions for converting between numpy arrays and Eigen data types
-template <typename R, int rows, int cols>
-Eigen::Matrix<R,rows,cols> numpy_to_eigen(){
-  
+// this one is tested and working correctly for 1d and 2d ndarrays
+Eigen::Matrix<real,Eigen::Dynamic,Eigen::Dynamic> numpy_to_eigen(const bp::object & in){
+  // get the shape
+  auto shape = in.attr("shape");
+  int rows = bp::extract<int>(shape[0]);
+  int cols;
+  bool b_one_d_array = false;
+  // determine if we are dealing with a 1d array
+  if(bp::len(shape) == 1){
+    cols = 1;
+    b_one_d_array = true;
+  }
+  else{
+    cols = bp::extract<int>(shape[1]);
+  }
+  // initialize return matrix
+  Eigen::Matrix<real,Eigen::Dynamic,Eigen::Dynamic> out(rows,cols);
+  for (int row = 0; row < rows; ++row)
+    {
+      // if we have a 1d array, we must reference its memory correctly
+      if(b_one_d_array){
+	  out(row,0) = bp::extract<float>(in[row]);
+      }
+      else{
+      for (int col = 0; col < cols; ++col)
+  	{
+	  // this is for the 2d array
+	  out(row,col) = bp::extract<float>(in[row][col]);
+  	}
+      }
+    }
+  return out;
 };
 
+// todo: a numpy_to_eigen without copying data
+// could be useful at testing time (need to care about lifetime of underlyng data!)
+
 // function returning a generic python object
-template <typename R, int rows, int cols>
-bp::object eigen_to_numpy(Eigen::Matrix<R,rows,cols>){
-  
+bp::object eigen_to_numpy(const Eigen::Matrix<real,Eigen::Dynamic,Eigen::Dynamic>& in){
+  // what is intp? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //npy_intp dims[2] = {1,2};
+
+  npy_intp dims[2] = {static_cast<npy_intp>(in.rows()),static_cast<npy_intp>(in.cols())};
+  std::cout<<"A"<<dims[0]<<dims[1]<<std::endl;
+  // in.data() is a const pointer so we need to resort to const_cast 
+  //  real * data = const_cast<real*>(in.data());
+  real data[dims[0]][dims[1]];
+  for (int row = 0; row < dims[0]; ++row)
+    {
+      for (int col = 0; col < dims[1]; ++col)
+	{
+	  data[row][col] = in(row,col);
+	}
+    }
+  std::cout<<"B"<<data[0][0]<<data[0][1]<<data[0][2]<<std::endl;
+  npy_intp d = 1;
+  /// this is now reduced to a basic example that SHOULD be working.
+  // need to google this further. Problem with som einitialization?
+  auto obj = PyArray_SimpleNew(1, &d, NPY_FLOAT);
+  std::cout<<"C"<<std::endl;
+
+  bp::handle<> handle(obj);
+  bp::numeric::array arr(handle);
+    std::cout<<"Ds"<<std::endl;
+
+  // return a COPY so that lifetime is managed in python
+  return arr.copy();
 };
 
 
@@ -36,16 +94,32 @@ class NumpyGPR : public GPR{
 public:
   NumpyGPR(int inputDim, int outputDim) : GPR(inputDim,outputDim){};
   // methods that will be part of the python interface and should be overridden:
-  // void AddTrainingData(bp::numeric::array new_input, bp::object new_output){
-  //   new_input.getshape();
-  // };
+  void AddTrainingData(const bp::numeric::array & new_input,const bp::numeric::array & new_output){
+    //GPR::AddTrainingData(numpy_to_eigen(new_input),numpy_to_eigen(new_output));
+    //numpy_to_eigen(new_input);
+  };
+
+  bp::object simple_test(const bp::numeric::array & in){
+    auto a = numpy_to_eigen(in);
+    std::cout<<a<<std::endl;
+    return eigen_to_numpy(a);
+  };
 
 };
 
-BOOST_PYTHON_MODULE(gaussian_process_regression){
-  // def("klas",klas_function);
+void simple_test2(const bp::numeric::array & in){
+    std::cout<<"hello, world"<<std::endl;    
+  };
 
+
+BOOST_PYTHON_MODULE(gaussian_process_regression){
+  bp::numeric::array::set_module_and_type("numpy", "ndarray");
+
+  //def("klas",klas_function);
+  bp::def("simple_test2",simple_test2);
   bp::class_<NumpyGPR>("GaussianProcessRegression",bp::init<int,int>())
     .def("SetHyperParams",&NumpyGPR::SetHyperParams)
+    .def("AddTrainingData",&NumpyGPR::AddTrainingData)
+    .def("simple_test",&NumpyGPR::simple_test)
     ;
 };
