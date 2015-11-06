c397093208674a8c6405ca0475a71dbeeb123ad2
Klas Kronander <klas.kronander@epfl.ch>
2015-11-06 12:57:14 +0100

    python imports now working

--- test_python/test_python_interface.py
+++ test_python/test_python_interface.py
@@ -1,10 +1,10 @@
 import numpy as np
-import gaussian_process_regression
+from gaussian_process_regression import GaussianProcessRegression
 import unittest
 
 class BasicTest(unittest.TestCase):
     def setUp(self):
-        self.gpr = gaussian_process_regression.GaussianProcessRegression(2, 1)
+        self.gpr = GaussianProcessRegression(2, 1)
 
     def test_add_data_one_by_one(self):
         inp, outp = np.random.randn(2,1), np.random.randn(1)
@@ -14,9 +14,6 @@ class BasicTest(unittest.TestCase):
         
 
 
-gp = gpr.GaussianProcessRegression(2, 1)
-inp, outp = np.random.randn(2,1), np.random.randn(1)
-print inp, outp
-gp.AddTrainingData(inp,outp)
-gp.AddTrainingData(inp,outp)
-gp.AddTrainingData(inp,outp)
+
+if __name__ == '__main__':
+    unittest.main()
